3f184f2071ed7ed33d1ec9d9e796ab81fff1add8
David Tran <decentvincent001@gmail.com>
2025-12-21 11:17:42 -0500

    Hook up bounded camera implementation to level 1

--- Assets/_Scripts/ParallaxController.cs
+++ Assets/_Scripts/ParallaxController.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 
 public class ParallaxController : MonoBehaviour {
-    private float _startPos;
+    private Vector2 _startPos;
     private Camera _camera;
     public float ParallaxMultiplier;
 
     private void Start() {
-        _startPos = transform.position.x;
+        _startPos = transform.position;
         _camera = Camera.main;
     }
 
     private void Update() {
         float parallaxDist = _camera.transform.position.x * ParallaxMultiplier;
 
-        transform.position = new Vector3(_startPos + parallaxDist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(_startPos.x + parallaxDist, _startPos.y + _camera.transform.position.y, transform.position.z);
     }
 }
